22159c48b10a340a0a43f9065ae51cb56c45ff95
Kojima <iwatowake@gmail.com>
2013-11-05 14:21:36 +0900

    デバッグマネージャ追加(今のところマウスモードのみ)

--- UnityProject/Assets/Script/GameObject/UI/UI_DicisionGauge.cs
+++ UnityProject/Assets/Script/GameObject/UI/UI_DicisionGauge.cs
@@ -4,10 +4,8 @@ using System.Collections;
 public class UI_DicisionGauge : SingletonMonoBehaviour<UI_DicisionGauge> {
 	
 	public	float			fillSpeed = 1.0f;
-	public	bool			mouseMode = false;
 	
 	private UIFilledSprite	sprite;
-	private	GameObject[]	palmObj;
 	private	bool			bIsOnButton = false;
 	private	bool			bFilled = false;
 	private	UI_Buttons		relatedButton;
@@ -24,30 +22,10 @@ public class UI_DicisionGauge : SingletonMonoBehaviour<UI_DicisionGauge> {
 	
 	void Start(){
 		sprite = gameObject.GetComponent<UIFilledSprite>();
-		palmObj = new GameObject[2];
-		
-		LeapUnityHandController hands = GameObject.Find("Leap Hands").GetComponent<LeapUnityHandController>();
-		
-		palmObj[0] = hands.m_palms[0];
-		palmObj[1] = hands.m_palms[0];
+
 	}
 	
-	void Update () {
-		if(mouseMode)
-		{
-			transform.localPosition = Input.mousePosition - new Vector3(Screen.width/2, Screen.height/2, 0);
-		}else{
-			for(int i=0; i<2; i++)
-			{
-				if(palmObj[i].GetComponent<LeapOrigamiCollider>().enabled)
-				{
-					Vector2 pos = palmObj[i].GetComponent<HandObjectController>().GetScreenPos();
-					transform.localPosition = new Vector3(pos.x, pos.y, 0) - new Vector3(Screen.width/2, Screen.height/2, 0);
-				}
-			}
-		//	transform.localPosition = GameObject.Find() - new Vector3(Screen.width/2, Screen.height/2, 0);
-		}
-		
+	void Update () {		
 		if(bIsOnButton)
 		{
 			if( (sprite.fillAmount >= 1.0f) && !bFilled)
