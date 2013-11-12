ede4babd46796fee23e04395bb6ea4c1287588d5
Joestar <yamaimo.yanagi@gmail.com>
2013-11-12 09:04:10 +0900

    エフェクト追加

---
+++ UnityProject/Assets/Script/GameObject/Effect/GameOverTransEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverTransEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.material.SetTexture("ParticleTexture",Resources.Load("Texture/rendertexture") as Texture2D);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/Script/GameObject/Effect/StringSelectEffect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StringSelectEffect : MonoBehaviour {
+
+	public Vector3				target = new Vector3(0,0,0);
+	
+	bool Checked = false;					
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.particleSystem.time > gameObject.particleSystem.duration / 2 && !Checked)
+		{			
+			ParticleSystem.Particle[] ParticleList = new ParticleSystem.Particle[gameObject.particleSystem.particleCount];
+			gameObject.particleSystem.GetParticles(ParticleList);
+			gameObject.particleSystem.emissionRate = 0;
+			gameObject.particleSystem.startSpeed = 0;
+			for(int i = 0; i < ParticleList.Length; ++i)
+			{
+				ParticleList[i].velocity = (target - ParticleList[i].position) / ParticleList[i].lifetime;
+			}
+			gameObject.particleSystem.SetParticles(ParticleList,gameObject.particleSystem.particleCount);
+			Checked = true;
+		}
+	
+	}
+}
