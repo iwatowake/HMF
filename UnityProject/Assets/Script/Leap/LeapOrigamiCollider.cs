049e5d92cb96efee7bc6c8ef7f61acc6aab54640
Joestar <yamaimo.yanagi@gmail.com>
2013-11-26 08:56:29 +0900

    テストシーン変更

--- UnityProject/Assets/Script/Leap/LeapOrigamiCollider.cs
+++ UnityProject/Assets/Script/Leap/LeapOrigamiCollider.cs
@@ -105,12 +105,12 @@ public class LeapOrigamiCollider : MonoBehaviour {
 				float Deg = Mathf.Acos(Angle)*180.0f/Mathf.PI;
 				Instantiate( ContactParticlePrefab, HitStartPos + Vec / 2.0f, Quaternion.AngleAxis( Deg, Camera.main.transform.forward ) );
 				PointParticle[1] = Instantiate( PointOneShotParticlePrefab, HitEndPos, Quaternion.identity ) as GameObject;
-<<<<<<< HEAD
+
 				PointParticle[0].particleSystem.emissionRate = 0;
 				OrigamiCutter.Cut( other.gameObject, HitStartPos, HitEndPos );
 				// レイヤー変更.
 				other.gameObject.layer = (int)LayerEnum.layer_OrigamiWait;
-=======
+
 				PointParticle[0].particleEmitter.emit = false;
 				//if( OrigamiCutter.Cut( other.gameObject, HitStartPos, HitEndPos ) ){
 				if( OrigamiMeshCutter.Cut( other.gameObject, HitStartPos, HitEndPos ) ){
@@ -120,7 +120,7 @@ public class LeapOrigamiCollider : MonoBehaviour {
 					Destroy( PointParticle[0] );
 					PointParticle[0] = null;
 				}
->>>>>>> 6839e28a80b41f5144798cf126c4dc89b29e4e13
+
 			}
 			else{
 				Destroy( PointParticle[0] );
