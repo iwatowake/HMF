951e77712acefbeb0bf1c8e87f1a5f3e3e52c300
Joestar <yamaimo.yanagi@gmail.com>
2013-12-06 09:02:12 +0900

    ゲームオーバー時の遷移エフェクト更新 遷移用レイヤーの追加 チュートリアルシーン追加

---
+++ UnityProject/Assets/Script/GameObject/State/TutorialState.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialState : StateBase {
+		
+	// 状態管理
+	enum STATE{
+		eEnter_Init = 0,
+		eEnter_Wait,
+		
+		eMain_Init,
+		eMain_Wait,
+		
+		eGameOver_Init,
+		eGameOver_Wait,
+		
+		eGameClear_Init,
+		eGameClear_Wait,
+		
+		eExit_Init,
+		eExit_Wait,
+		
+		eChangeState_ToInGame
+	}
+	
+	private STATE	state		= STATE.eEnter_Init;	//!< 状態管理用
+	
+	protected override void Start ()
+	{
+		base.Start ();
+	}
+	
+	public override void Exec ()
+	{
+		switch(state)
+		{
+			// インゲーム　状態入り
+		case STATE.eEnter_Init:
+			CRI_SoundManager_2D.Instance.PlayBGM(BGM_ID.INGAME);
+
+			Debug.Log("Enter_Tutorial");
+			Game_CityLayer.Instance.CityLayerEnable(0,true);
+//			UI_TimeCounter.Instance.SetEnable(false);
+			UI_TentionGauge.Instance.SetEnable(false);
+			FadeIn(1.5f);			
+			state++;
+			break;
+		case STATE.eEnter_Wait:
+			Debug.Log("enter_wait");
+			break;
+			
+			// インゲーム　入力待ち
+		case STATE.eMain_Init:
+			Debug.Log("main_init");
+//			UI_TimeCounter.Instance.SetEnable(true);
+			UI_TentionGauge.Instance.SetEnable(true);
+			state++;
+			break;
+		case STATE.eMain_Wait:
+//			UI_TimeCounter.Instance.Exec();
+			break;
+			
+		case STATE.eGameOver_Init:
+			state++;
+			break;
+		case STATE.eGameOver_Wait:
+			state = STATE.eExit_Init;
+			break;
+			
+		case STATE.eGameClear_Init:
+			state++;
+			break;
+		case STATE.eGameClear_Wait:
+			state = STATE.eExit_Init;
+			break;
+			
+			// インゲーム　状態離脱
+		case STATE.eExit_Init:
+			FadeOut(1.5f);
+			GameObject.Find("UI_InGame").SetActive(false);
+			state++;
+			break;
+		case STATE.eExit_Wait:
+			break;
+			
+			// 次の状態へ
+		case STATE.eChangeState_ToInGame:
+			StateController.Instance.ChangeState(E_STATE.InGame);
+			break;
+		}
+	}
+	
+	protected override void OnDestruct ()
+	{
+		
+	}
+	
+	
+	//! 状態入り　初期化
+	private void Enter_Init(){
+		
+	}
+	//! 状態入り　待機
+	private void Enter_Wait(){
+		
+	}
+	
+	
+	//! メイン処理　初期化
+	private void Main_Init(){
+		
+	}
+	//! メイン処理　待機
+	private void Main_Wait(){
+		
+	}
+	
+	override protected void OnCompleteFade(){
+		state++;
+	}
+	
+	public void EndText()
+	{
+		state = STATE.eExit_Init;
+	}
+}
---
+++ UnityProject/Assets/Script/GameObject/TutorialText.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialText : MonoBehaviour {
+	public string[] text;
+	public GameObject tutorialState;
+	
+	int currentNo = 0;
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.localScale = Vector3.zero;
+		OpenText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentNo < text.Length)
+		{
+			TextMesh tm = (TextMesh)gameObject.GetComponent("TextMesh");
+			tm.text = text[currentNo];
+		}
+	}
+
+	void OpenText()
+	{
+		iTweenEvent.GetEvent(gameObject,"OpenTextTween").Play();
+	}
+	
+	void IdleText()
+	{
+		iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();				
+	}
+	
+	void CloseText()
+	{
+		iTweenEvent.GetEvent(gameObject,"CloseTextTween").Play();		
+	}
+	
+	void EndTutorial()
+	{
+		tutorialState.GetComponent<TutorialState>().EndText();
+	}
+
+	void NextText()
+	{
+		currentNo++;
+		if(currentNo >= text.Length)
+			EndTutorial();
+		else
+			OpenText();
+	}
+	
+}
