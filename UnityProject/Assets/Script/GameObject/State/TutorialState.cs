9e634061d95af4c32eb09623a4d6e1845d3bebd5
Joestar <yamaimo.yanagi@gmail.com>
2013-12-17 10:27:01 +0900

    サンプルムービー追加

--- UnityProject/Assets/Script/GameObject/State/TutorialState.cs
+++ UnityProject/Assets/Script/GameObject/State/TutorialState.cs
@@ -3,92 +3,72 @@ using System.Collections;
 
 public class TutorialState : StateBase {
 		
-	// 状態管理
+	// 状態管理.
 	enum STATE{
 		eEnter_Init = 0,
 		eEnter_Wait,
 		
 		eMain_Init,
 		eMain_Wait,
-		
-		eGameOver_Init,
-		eGameOver_Wait,
-		
-		eGameClear_Init,
-		eGameClear_Wait,
-		
+				
 		eExit_Init,
 		eExit_Wait,
 		
 		eChangeState_ToInGame
 	}
 	
-	private STATE	state		= STATE.eEnter_Init;	//!< 状態管理用
-	private OrigamiController OrigamiControllerScript = null;
+	private STATE	state		= STATE.eEnter_Init;	//!< 状態管理用.
 	
 	public	int	OriTime = 300;
 	
 	protected override void Start ()
 	{
 		base.Start ();
-		OrigamiControllerScript = GameObject.Find( "OrigamiController" ).GetComponent<OrigamiController>();
 	}
 	
 	public override void Exec ()
 	{
 		switch(state)
 		{
-			// インゲーム　状態入り
+			// インゲーム　状態入り.
 		case STATE.eEnter_Init:
 			CRI_SoundManager_2D.Instance.PlayBGM(BGM_ID.INGAME);
 
 			Debug.Log("Enter_Tutorial");
 			Game_CityLayer.Instance.CityLayerEnable(0,true);
 //			UI_TimeCounter.Instance.SetEnable(false);
-			UI_TentionGauge.Instance.SetEnable(false);
+//			UI_TentionGauge.Instance.SetEnable(false);
 			FadeIn(1.5f);			
-			state++;
+			state = STATE.eEnter_Wait;
+			
 			break;
 		case STATE.eEnter_Wait:
-			Debug.Log("enter_wait");
-			state++;
 			break;
 			
-			// インゲーム　入力待ち
+			// インゲーム　入力待ち.
 		case STATE.eMain_Init:
-			Debug.Log("main_init");
 //			UI_TimeCounter.Instance.SetEnable(true);
-			UI_TentionGauge.Instance.SetEnable(true);
-			state++;
-			PlayOrigami();
+//			UI_TentionGauge.Instance.SetEnable(true);
+			state = STATE.eMain_Wait;
+//			PlayOrigami();
 			break;
+
 		case STATE.eMain_Wait:
 //			UI_TimeCounter.Instance.Exec();
-			break;
-			
-		case STATE.eGameOver_Init:
-			state++;
-			break;
-		case STATE.eGameOver_Wait:
-			state = STATE.eExit_Init;
-			break;
-			
-		case STATE.eGameClear_Init:
-			state++;
-			break;
-		case STATE.eGameClear_Wait:
-			state = STATE.eExit_Init;
+			Debug.Log("main_wait");
 			break;
 			
 		case STATE.eExit_Init:
 			FadeOut(1.5f);
+			Debug.Log("Exit");
 			GameObject.Find("UI_InGame").SetActive(false);
-			state++;
+			state = STATE.eExit_Wait;
 			break;
+			
 		case STATE.eExit_Wait:
 			break;
 			
-			// 次の状態へ
+			// 次の状態へ.
 		case STATE.eChangeState_ToInGame:
 			StateController.Instance.ChangeState(E_STATE.InGame);
 			break;
@@ -101,21 +81,21 @@ public class TutorialState : StateBase {
 	}
 	
 	
-	//! 状態入り　初期化
+	//! 状態入り　初期化.
 	private void Enter_Init(){
 		
 	}
-	//! 状態入り　待機
+	//! 状態入り　待機.
 	private void Enter_Wait(){
 		
 	}
 	
 	
-	//! メイン処理　初期化
+	//! メイン処理　初期化.
 	private void Main_Init(){
 		
 	}
-	//! メイン処理　待機
+	//! メイン処理　待機.
 	private void Main_Wait(){
 		
 	}
@@ -127,10 +107,6 @@ public class TutorialState : StateBase {
 	public void EndText()
 	{
 		state = STATE.eExit_Init;
-	}
-	
-	public void PlayOrigami()
-	{
-		OrigamiControllerScript.CreateOrigami(WAKU.LEVEL_1_1,OriTime);
+		Debug.Log("EndText");
 	}
 }
--- UnityProject/Assets/Script/GameObject/TutorialText.cs
+++ UnityProject/Assets/Script/GameObject/TutorialText.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class TutorialText : MonoBehaviour {
 	public string[] text;
 	public GameObject tutorialState;
+	public GameObject tutorialMovie;
 	
+	private OrigamiController OrigamiControllerScript = null;
+
 	int currentNo = 0;
 	
 	// Use this for initialization
 	void Start () {
+		OrigamiControllerScript = GameObject.Find( "OrigamiController" ).GetComponent<OrigamiController>();
 		gameObject.transform.localScale = Vector3.zero;
 		OpenText();
+		Debug.Log("Text" + text.Length.ToString());
+		
+		MovieTexture tex = (MovieTexture)tutorialMovie.renderer.material.mainTexture;
+		tex.Play();
 	}
 	
 	// Update is called once per frame
@@ -21,7 +29,12 @@ public class TutorialText : MonoBehaviour {
 			tm.text = text[currentNo];
 		}
 	}
-
+	
+	void Restart()
+	{
+		currentNo = 1;
+	}
+	
 	void OpenText()
 	{
 		iTweenEvent.GetEvent(gameObject,"OpenTextTween").Play();
@@ -29,7 +42,40 @@ public class TutorialText : MonoBehaviour {
 	
 	void IdleText()
 	{
-		iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();				
+		switch(currentNo)
+		{
+		// 折るための始点と終点の線を引く説明.
+		case 5:
+//			if(OrigamiControllerScript.GetState() == OrigamiUpdate.STATE.FOLD_SELECT)
+//				NextText();
+			iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();		
+			break;
+
+		// 折る方向の選択の説明.
+		case 6:
+//			if(OrigamiControllerScript.GetState() == OrigamiUpdate.STATE.CUT)
+//				NextText();
+			iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();		
+			break;
+
+		// 戻る機能の説明.
+		case 7:
+//			if(OrigamiControllerScript.GetState() == OrigamiUpdate.STATE.FOLD_SELECT)
+//				NextText();
+			iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();		
+			break;
+			
+		// 拍手の説明.
+		case 8:
+			iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();		
+//			if(OrigamiControllerScript.GetActiveFlg() == false)
+//				NextText();
+			break;
+
+		default:
+			iTweenEvent.GetEvent(gameObject,"IdleTextTween").Play();		
+			break;
+		}
 	}
 	
 	void CloseText()
@@ -44,7 +90,7 @@ public class TutorialText : MonoBehaviour {
 	
 	void PlayOrigami()
 	{
-		tutorialState.GetComponent<TutorialState>().EndText();		
+//		OrigamiControllerScript.CreateOrigami(WAKU.LEVEL_1_1,OriTime);
 	}
 	
 	void NextText()
@@ -54,6 +100,9 @@ public class TutorialText : MonoBehaviour {
 			EndTutorial();
 		else
 			OpenText();
+		
+		if(currentNo == 5)
+			OrigamiControllerScript.CreateOrigami(WAKU.LEVEL_1_1,1000);
 	}
 	
 }
